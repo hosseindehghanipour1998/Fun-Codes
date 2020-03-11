aafb211c211303883a08b8cf8759904502008e72
Hossein <hossein.dehghanipour1998@gmail.com>
2020-03-12 02:08:13 +0330

    A simple form of converter-> needs to extract more information

---
+++ Python/VCF Converter/VCF Converter.py
@@ -0,0 +1,47 @@
+
+
+def openAndReturnContents(fileName):
+    fileFullName = fileName + ".vcf"
+    fileObject = open(fileFullName,"r")
+    content = fileObject.read().split("BEGIN:VCARD")
+    fileObject.close()
+    return content    
+
+def convertData(rawContentArray):
+    processedContacts = []
+    i = 0 
+    
+    for contact in rawContentArray:
+        details = contact.split('\n')
+        print(str(i) +"-" + str(details) )
+        try:     
+            nameList = details[3]
+            tel =  details[4]
+            nameTelTuple = ( nameList , tel)
+            processedContacts.append(nameTelTuple)
+            i += 1     
+        except:   
+            print("Invalid format for Contact")
+    return processedContacts
+
+
+def writeToFile(fileName,arrayList):
+    fileFullName = fileName + ".txt"
+    fileObject = open(fileFullName,"w")
+    print(arrayList)
+    for item in arrayList:
+        print(item)
+        fileObject.write("Name: " + item[0] +'\n')
+        fileObject.write("Phone: " + item[1] +'\n')
+        fileObject.write("==================== \n")
+
+def run():
+    # C:\Users\hosse\Desktop 
+    fileName ="C:\\Users\\hosse\\Desktop\\Contacts"
+    print("Hello")
+    content = openAndReturnContents(fileName)
+    print("World")
+    processedData = convertData(content)
+    writeToFile("ContactInTextFile" ,processedData )
+    
+run()
