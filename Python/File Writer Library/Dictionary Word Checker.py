b1e7dc98ef7d4464c97826328dc526051cff34be
Hossein <hossein.dehghanipour1998@gmail.com>
2020-05-06 04:21:48 +0430

    reshift

--- Python/File Writer Library/Dictionary Word Checker.py
+++ Python/File Writer Library/Dictionary Word Checker.py
@@ -22,37 +22,37 @@ def start():
         print(str(i) + ": ")
         word = input()
         # ====================================
-        try :
-            #it's a Number
-            word + 1
-            #delete(word-1,wordFilename)
-            #system('cls')
-            #continue
-        except :    
-            # ====================================
-            if(word == "q"):
-                break
-            # ====================================
-            elif(word == "p"):
-                listOfWords = writer.readFile()
-                print("\n=====================================================\n")
-                print(str(listOfWords) + "\n")
-                print("\n=====================================================\n")
-            # ====================================
-            elif (word == "b"):
-                enteredPass = input("Password To get Backup : ")
-                if ( enteredPass == str(password) ):
-                    writer.createBackup()
-            # ====================================
-            elif(word =="c"):
-                enteredPass = input("Password To Delete Everything : ")
-                if ( enteredPass == str(password) ):
-                    writer.clearFile()
 
-            # ====================================
-            else:
-                if( writer.appendNoneRepeated(word) == False):
-                    print("Word Already Added")
+        #it's a Number
+        word + 1
+        #delete(word-1,wordFilename)
+        #system('cls')
+        #continue
+
+        # ====================================
+        if(word == "q"):
+            break
+        # ====================================
+        elif(word == "p"):
+            listOfWords = writer.readFile()
+            print("\n=====================================================\n")
+            print(str(listOfWords) + "\n")
+            print("\n=====================================================\n")
+        # ====================================
+        elif (word == "b"):
+            enteredPass = input("Password To get Backup : ")
+            if ( enteredPass == str(password) ):
+                writer.createBackup()
+        # ====================================
+        elif(word =="c"):
+            enteredPass = input("Password To Delete Everything : ")
+            if ( enteredPass == str(password) ):
+                writer.clearFile()
+
+        # ====================================
+        else:
+            if( writer.appendNoneRepeated(word) == False):
+                print("Word Already Added")
     print("Done")
     input()
 
